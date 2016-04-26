dfa11b844d2d71bff74264910088638beb2523dc
cgkaczenski <kzpulaski@gmail.com>
2016-04-26 11:45:08 -0500

    posting and saving text

--- backend/API.js
+++ backend/API.js
@@ -64,7 +64,7 @@ var Router = require('../frontend/js/lib/Router')();
 Router
 .add('api/version', function(req, res) {
   response({
-    version: '0.2'
+    version: '0.3'
   }, res);
 })
 .add('api/user/logout', function(req, res) {
@@ -296,6 +296,60 @@ Router
   } else {
     error('You must be logged in to use this method.', res);
   }
+})
+.add('api/content', function(req, res) {
+  var user;
+  if(req.session && req.session.user) {
+    user = req.session.user;
+  } else {
+    error('You must be logged in in order to use this method.', res);
+    return;
+  }
+  switch(req.method) {
+    case 'POST':
+      var uploadDir = __dirname + '/../static/uploads/';
+      var formidable = require('formidable');
+      var form = new formidable.IncomingForm();
+      form.multiples = true;
+      form.parse(req, function(err, data, files) {
+        if(!data.text || data.text === '') {
+          error('Please add some text.', res);
+        } else {
+          var processFiles = function(userId, callback) {
+            if(files.files) {
+              var fileName = userId + '_' + files.files.name;
+              var filePath = uploadDir + fileName;
+              fs.rename(files.files.path, filePath, function(err) {
+                if(err) throw err;
+                callback(fileName);
+              });
+            } else {
+              callback();
+            }
+          };
+          var done = function() {
+            response({
+              success: 'OK'
+            }, res);
+          }
+          getDatabaseConnection(function(db) {
+            getCurrentUser(function(user) {
+              var collection = db.collection('content');
+              data.userId = user._id.toString();
+              data.userName = user.firstName + ' ' + user.lastName;
+              data.date = new Date();
+              processFiles(user._id, function(file) {
+                if(file) {
+                  data.file = file;
+                }
+                collection.insert(data, done);
+              });
+            }, req, res);
+          });
+        }
+      });
+    break;
+  };
 })
 .add(function(req, res) {
   response({
--- frontend/js/controllers/Home.js
+++ frontend/js/controllers/Home.js
@@ -1,10 +1,35 @@
+var ContentModel = require('../models/Content');
+
 module.exports = Ractive.extend({
   template: require('../../tpl/home'),
   components: {
     navigation: require('../views/Navigation'),
     appfooter: require('../views/Footer')
   },
+  data:{
+  	posting: true
+  },
   onrender: function() {
-    console.log('Home page rendered');
-  }
+    if(userModel.isLogged()) {
+    	var ContentModel = require('../models/Content');
+		var model = new ContentModel();
+		var self = this;
+		this.on('post', function() {
+		  model.create({
+		    text: this.get('text')
+		  }, function(error, result) {
+		    self.set('text', '');
+		    if(error) {
+		      self.set('error', error.error);
+		    } else {
+		      self.set('error', false);
+		      self.set('success', 'The post is saved successfully.<br />What about adding another one?');
+		    }
+		  });
+		});
+
+		} else {
+			this.set('posting', false);
+		}
+  	}
 });
